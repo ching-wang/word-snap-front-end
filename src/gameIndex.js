b32b7e6358d38f2a78bc33a4a399d64497c1c1fb
Qing <chingwang.qw@gmail.com>
2020-01-19 20:43:59 +0000

    fetch pairs data

--- src/gameIndex.js
+++ src/gameIndex.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
 import "./App.css";
 import GameBoard from "./components/GameBoard";
+import { getPairs } from "./API";
 
 class GameIndex extends Component {
+  state = {
+    pairs: []
+  };
+
+  componentDidMount() {
+    document.body.style.backgroundColor = "orange";
+    getPairs().then(pairs =>
+      this.setState({
+        pairs
+      })
+    );
+  }
+
   render() {
+    const { pairs } = this.state;
+
     return (
       <div>
-        <GameBoard />
+        <GameBoard pairsToRender={pairs} />
       </div>
     );
   }
