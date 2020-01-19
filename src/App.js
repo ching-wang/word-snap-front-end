61170cc44f1e13b78629e223ae2d136563762cf6
Anastasia Pankina <siapankina@gmail.com>
2020-01-19 19:40:44 +0000

    added fetch request for newgame response is saved to state players array

--- src/App.js
+++ src/App.js
@@ -3,15 +3,30 @@ import "./App.css";
 import Header from "./components/Header";
 import GameIndex from "./gameIndex";
 import LoginForm from "./components/LoginForm";
-
+const NewGame = "http://localhost:3001/newgame";
 class App extends Component {
   state = {
     player_1: "",
     player_2: "",
-    gameOn: false
+    gameOn: false,
+    players: {}
   };
   login = (player_1, player_2) => {
+    fetch(NewGame, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ players: { user_1: player_1, user_2: player_2 } })
+    })
+      .then(resp => resp.json())
+      .then(data => {
+        console.log(data);
+        this.setState({ players: data });
+      });
+
     console.log(`player 1: ${player_1} player 2: ${player_2}`);
+
     this.setState({ gameOn: !this.state.gameOn });
   };
 
